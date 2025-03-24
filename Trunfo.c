3d0ca624d9ce29001e50a637c324ec78cc24dedc
gosht-v <tecnicomsstudio@gmail.com>
2025-03-24 20:31:27 -0300

    Create Trunfo.c

---
+++ Trunfo.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+  int main(){
+      char estado;                    // letra do Estado (A a H)
+      char codigo[A01];               // Código da carta
+      char cidade[nomecidade];        // Nome da cidade
+      int populacao;                  // Populaçao da cidade
+      float area;                     // Área da cidade em Km²
+      float pib;                      // PIB da cidade
+      int pontosturisticos;           // Número de pontos turísticos na cidade
+
+      
